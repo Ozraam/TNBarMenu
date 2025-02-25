2eaf6483f73309a26c68ea4d69a8f815589d33ad
Tristan <ardouin.t@france-elevateur.fr>
2025-02-25 16:51:35 +0100

    begining of the app

---
+++ src/lib/OptionSelector.svelte
@@ -0,0 +1,3 @@
+<div class="flex p-3 justify-between bg-amber-600/20 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100">
+    a
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { classList } from "$lib/classList";
+    import OptionSelector from "$lib/OptionSelector.svelte";
+    import ImageViewer from "$lib/preview/ImageViewer.svelte";
+    import TextPreview from "$lib/preview/TextPreview.svelte";
+</script>
+
+<svelte:body use:classList={["min-h-screen", "flex", "flex-col"]} />
+
+
+<header class="flex justify-between m-3 p-3 bg-amber-600/20 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100">
+    <h1>
+        Menu Bar'bare
+    </h1>
+
+    <nav>
+        <ul class="flex gap-6">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<main class="p-3 flex h-full flex-1">
+    <OptionSelector />
+
+    <div>
+        <ImageViewer />
+
+        <ImageViewer />
+
+        <TextPreview />
+
+        <TextPreview />
+    </div>
+</main>
